56f3c5a4415b620489b363d7dfd7a57f6ba4fb2b
John M. Daniel <imjohnmdaniel@ce-v.com>
2023-12-26 16:05:59 -0500

    chore: check in of reference project

---
+++ referenceproject/bin/resetScratchOrg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# validation stage
+
+# initializeStage
+sf org delete scratch --no-prompt
+sf org create scratch --wait 30 --duration-days 1 --set-default --definition-file config/foobar-project-scratch-def.json --alias foobar
+sf data record update --sobject Organization --where "Name='FOOBAR Company'" --values "TimeZoneSidKey='America/New_York'" 
+sf data record update --sobject User --where "Name='User User'" --values "TimeZoneSidKey='America/New_York'" 
+
+# processResoucesStage
+sf toolbox package dependencies install 
+
+# compilationStage
+sf project deploy start --ignore-conflicts
+
+# testingStage
+# sfdx force:apex:execute -f scripts/configureForceDIBindingCache.apex
+# sfdx force:source:deploy --sourcepath sfdx-source/unpackaged-post-deployment
+# sfdx force:user:permset:assign -n FooBarAccess 
+# sfdx sfdmu:run -s csvfile -u foobar -p data/Accounts
