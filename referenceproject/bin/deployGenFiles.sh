19e7c951fac6541b715c73453d16a060be13bbb4
John M. Daniel <imjohnmdaniel@ce-v.com>
2024-01-11 11:45:09 -0500

    chore: updates to various files

---
+++ referenceproject/bin/deployGenFiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# clean up sfdx-source/untracked/ folder
+rm -f sfdx-source/untracked/*.cls
+rm -f sfdx-source/untracked/*.trigger
+rm -f sfdx-source/untracked/*.xml
+
+# move all generated files to sfdx-source/untracked/
+mv generated-files/* sfdx-source/untracked/
+
+# deploy code
+sf project deploy start --ignore-conflicts
